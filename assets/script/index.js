699a15feea955384e54f105b3f1410908d642cc5
RAguirreGiraldo <raguirregiraldo@gmail.com>
2022-11-25 15:36:02 -0600

    update validate javaScript

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,16 +1,37 @@
 'use strict';
 
-import Contact from './Contact.js'
+import Contact from './contact'
 
 document.getElementById('submit-btn').addEventListener("click", newContact);
 
-function newContact(){
-    let data = document.getElementById('entrytext').value;
-    alert('output:' + JSON.stringify(data));
+
+function validateQuantity(data) {
+    let isValid = 'Please type the requested data separated by commas';
+    let arrayData = data.split(",");    
+
+    
+    if(arrayData.length <3){
+        isValid= '';
+    } else {
+        //validar email
+        let position = arrayData[2].search("@");
+        if (position == -1){
+            isValid= 'The email is not valid, please check it.';
+        }
+    }
+    return isValid != '' ? false : true;
+
 }
 
-const data = document.getElementById('entrytext').value;
-console.log(data);
 
-let c1= new Contact('John','City','email');
-console.log(c1);
+function newContact(){
+    
+    let data = document.getElementById('entrytext').value;
+    //validate data
+    if (validateQuantity(data)){
+        console.log('registro valido');        
+    } else {
+        console.log('errado');
+    }
+}
+
